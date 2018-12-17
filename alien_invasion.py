73461ac1a91804baf0ab688636f32fa3a951774b
mfdukh008 <664040720@qq.com>
2018-12-17 11:27:49 +0800

    12.17

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
-
+from alien import  Alien
 def run_game():
     pygame.init()
     ai_settings = Settings()
@@ -17,17 +17,21 @@ def run_game():
     ship = Ship(ai_settings,screen)
 
     bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+    # alien = Alien(ai_settings,screen)
 
     while True:
         ship.blitme()
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        bullets.update()
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
+        gf.update_bullets(aliens,bullets)
+        # for bullet in bullets.copy():
+        #     if bullet.rect.bottom <= 0:
+        #         bullets.remove(bullet)
         # print(len(bullets))
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_aliens(ai_settings,aliens)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
         # screen.fill(ai_settings.bg_color)
         # pygame.display.flip()
 
